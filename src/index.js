4cba11feae5a9b4751ea48fc82cd94e54da027bb
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-12-16 22:49:25 +0200

    chore(devs): added epitacio to dev list

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const Client = require('./structures/Client');
 
 const client = new Client({
-  devs: ['422109629112254464', '266132370426429440'],
+  devs: ['422109629112254464', '266132370426429440', '373206800196960268'],
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
   messageCacheLifetime: 60,
   messageSweepInterval: 120,
