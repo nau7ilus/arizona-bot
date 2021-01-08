d31063f84094514d062719359268d937b0b86113
Sejapoe <57502189+Fiori-S@users.noreply.github.com>
2021-01-08 22:02:21 +0300

    add message.txt support

--- src/commands/Rules/editrules.js
+++ src/commands/Rules/editrules.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const https = require('https');
 const Command = require('../../structures/Command');
 const rulesConfig = require('../../utils/config').rulesConfig;
 
@@ -48,9 +50,12 @@ module.exports = class extends Command {
 
     if (!content || content.first().content === '-') return;
 
+    const contentText =
+      content.first().attachments && content.first().attachments.size !== 0
+        ? await getAttachmentContent(content.first().attachments.first())
+        : content.first().content.trim();
+    
     await content.first().delete();
-
-    const contentText = content.first().content.trim();
 
     const fields = [];
     let current = 0;
@@ -108,3 +113,43 @@ module.exports = class extends Command {
     }
   }
 };
+
+function getAttachmentContent(attachment) {
+  return new Promise((resolve, reject) => {
+    const path = `tmp_${attachment.id}.txt`;
+    const file = fs.createWriteStream(path, { flags: 'wx' });
+
+    const request = https.get(attachment.url, response => {
+      if (response.statusCode === 200) {
+        response.pipe(file);
+      } else {
+        file.close();
+        fs.unlinkSync(path);
+        reject(new Error(`Server responded with ${response.statusCode}: ${response.statusMessage}`));
+      }
+    });
+
+    request.on('error', err => {
+      file.close();
+      fs.unlinkSync(path);
+      reject(err);
+    });
+
+    file.on('finish', () => {
+      const content = fs.readFileSync(path);
+      fs.unlinkSync(path);
+      resolve(content.toString());
+    });
+
+    file.on('error', err => {
+      file.close();
+
+      if (err.code === 'EEXIST') {
+        reject(new Error('File already exists'));
+      } else {
+        fs.unlinkSync(path);
+        reject(err);
+      }
+    });
+  });
+}
