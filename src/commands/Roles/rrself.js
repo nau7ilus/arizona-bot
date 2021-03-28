904cd286aa260201edd169abf664e9930ba0d8da
Влад <termuxlamer@gmail.com>
2021-03-28 15:34:16 +0200

    feat(rrself): added some const

--- src/commands/Roles/rrself.js
+++ src/commands/Roles/rrself.js
@@ -3,7 +3,6 @@
 const { MessageEmbed } = require('discord.js');
 const Command = require('../../structures/Command');
 const { sendErrorMessage } = require('../../utils');
-const talkedRecently = new Set();
 const rrselfConfig = require('../../utils/config').rrselfConfig;
 
 module.exports = class extends Command {
@@ -15,9 +14,9 @@ module.exports = class extends Command {
   async run({ args, message }) {
     
     message.delete()
-    const settings = rrselfConfig[message.guild.id]
     const rolesToRemove = config.roles.filter(r => message.member.roles.cache.has(r));
     const unsuccessRoles = [];
+    const settings = rrselfConfig[message.guild.id]
 
     rolesToRemove.forEach(r => {if (roles.length === 0) {
       sendErrorMessage({
@@ -40,10 +39,5 @@ unsuccessRoles.push({ id: r, reason: err.message });
         .setTitile('** :white_check_mark: | Успешное выполнение команды!**')
         .setDescription(`Вы успешно сняли с себя роли: ${roles.join()}`);
     const msg = await message.channel.send(embed)
-
-  }
-  async removeRoles(message,author,rolesToRemove, editMsg = false) {
-    const successRoles = [];
-    const ejectRoles = [];
-  }
 }
+};
